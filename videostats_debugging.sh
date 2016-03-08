8b36a65bc2ff43a1bb89750cd5fea1db0d54c721
Jon Morehouse <morehousej09@gmail.com>
2016-03-08 00:41:43 -0800

    wip

--- videostats_debugging.sh
+++ videostats_debugging.sh
@@ -1,7 +1,22 @@
 root@videostats-stage01:~# grep "what is broken?" /buzzfeed/video_stats/video_stats/celery/log/events.log
+nice try
+
 root@videostats-stage01:~# grep "what is broken?" /var/log/django/*log
+keep looking
+
 root@videostats-stage01:~# grep "what is broken?" /var/log/apache2/*log
+hahaha
+
 root@videostats-stage01:~# grep "what is broken?" /var/log/mongo/*log
+you sure this is the right computer?
+
 root@videostats-stage01:~# grep "what is broken?" /**/*
+no comment
+
 root@videostats-stage01:~# grep "please work?" /**/*
+videostats is going to shut down now
+
 root@videostats-stage01:~# grep "c'mon man its 3am" /**/*
+good bye
+
+
